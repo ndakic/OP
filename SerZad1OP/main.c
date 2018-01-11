0e997857aca34a2e1a42cf1706d47c59fa6aa9d4
Nikola Dakic <nikoladakic@uns.ac.rs>
2018-01-11 12:56:58 +0100

    Ser1

--- SerZad1OP/main.c
+++ SerZad1OP/main.c
@@ -3,6 +3,9 @@
 
 #define factorB 3 // blocking factor
 
+#define true 1
+#define false 0
+
 typedef struct{
     char date[10]; // 01-01-2018
     char time[5]; // 12:15
@@ -26,6 +29,16 @@ typedef struct{
     Prisoner prisoners[factorB];
 }Block;
 
+
+void saveBlock(TFile *file, int blockAddress, Block *block);
+void readBlock(TFile *file, int blockAddress, Block *block);
+void createFile();
+void chooseFile(TFile *file);
+
+void printPrisoner(Prisoner prisoner);
+void printAll(TFile *file);
+
+int search(TFile *file, char *id);
 
 
 void saveBlock(TFile *file, int blockAddress, Block *block){
@@ -62,17 +75,6 @@ void readBlock(TFile *file, int blockAddress, Block *block){
 
 }
 
-void printPrisoner(Prisoner prisoner){
-
-    puts("-------------------------------------------------------");
-    printf("Record Number: %d\n", prisoner.recordNumber);
-    printf("Prisoner id: %s\n", prisoner.id);
-    printf("Cell id: %s\n", prisoner.cellId);
-    printf("Length Of Sentence: %d\n", prisoner.lengthOfSentence);
-    puts("-------------------------------------------------------");
-
-}
-
 void createFile(){
 
     TFile file;
@@ -91,24 +93,114 @@ void createFile(){
 
 }
 
+void chooseFile(TFile *file){
+
+    file->file = fopen("test.txt", "rb+");
+
+     if(file->file == NULL)
+        return;
+
+    puts("File is active!");
+
+}
+
+void printPrisoner(Prisoner prisoner){
+
+    puts("-------------------------------------------------------");
+    printf("Record Number: %d\n", prisoner.recordNumber);
+    printf("Prisoner id: %s\n", prisoner.id);
+    printf("Cell id: %s\n", prisoner.cellId);
+    printf("Length Of Sentence: %d\n", prisoner.lengthOfSentence);
+    puts("-------------------------------------------------------");
+
+}
+
+void printAll(TFile *file){
+
+    int block_position = 0;
+    int i;
+
+    Block block;
+
+    while(true){
+
+        readBlock(file, block_position, &block);
+
+        for(i=0; i < factorB; i++){
+
+            if(strcmp(block.prisoners[i].id, "*") == 0)
+                return;
+
+            printPrisoner(block.prisoners[i]);
+
+        }
+    }
+
+}
+
+int search(TFile *file, char *id){
+
+    int block_position = 0;
+    int i;
+
+    Block block;
+
+    while(true){
+
+        readBlock(file, block_position, &block);
+
+        for(i=0; i < factorB; i++){
+
+            if(strcmp(block.prisoners[i].id, "*") == 0){
+                puts("End of File");
+                return 0;
+            }
+
+            if(strcmp(block.prisoners[i].id, id) == 0){
+                puts("The prisoner was found!");
+                return 1;
+            }
+
+        }
+
+        block_position++;
+
+    }
+
+}
+
+
 
 int main()
 {
     TFile file;
     file.file = NULL;
 
-    //createFile();
+    while(true){
+        printf("\n===================\n");
+        printf("1. Create a File\n");
+        printf("2. Choose a File\n");
+
+        printf("0. Exit\n");
+        printf(">>");
+
+        int option;
+        scanf("%d", &option);
+
+        switch(option){
+
+            case 1: createFile(&file); break;
+
+            case 2: chooseFile(&file); break;
+
+            case 3: printAll(&file); break;
 
 
-    file.file = fopen("test.txt", "rb");
+            case 0: exit(0); break;
 
-    Block block;
+        }
 
-    readBlock(&file.file, 0, &block);
+    };
 
-    printPrisoner(block.prisoners[0]);
-
-
-    printf("End\n");
     return 0;
 }
